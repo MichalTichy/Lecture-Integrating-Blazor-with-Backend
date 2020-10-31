fa3455dc328c389a8600b1e676db7e89ec927f44
Michal Tichý <michal.tichy@riganti.cz>
2020-10-31 20:47:34 +0100

    Added Installer

---
+++ CookBook.WEB.BL/BLWebInstaller.cs
@@ -0,0 +1,22 @@
+﻿using CookBook.ApiClients;
+using CookBook.BL.Common.Facades;
+using CookBook.Common.Installers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CookBook.WEB.BL
+{
+    public class BLWebInstaller : IInstaller
+    {
+        public void Install(IServiceCollection serviceCollection)
+        {
+            serviceCollection.Scan(selector =>
+                selector.FromCallingAssembly()
+                        .AddClasses(classes => classes.AssignableTo<IAppFacade>())
+                        .AsSelfWithInterfaces()
+                        .WithTransientLifetime());
+            
+            serviceCollection.AddSingleton<IIngredientClient, IngredientClient>();
+            serviceCollection.AddSingleton<IRecipeClient, RecipeClient>();
+        }
+    }
+}
